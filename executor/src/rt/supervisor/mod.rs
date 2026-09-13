a27db421e17c5d534f6955302df3f5357d572848
kp2pml30 <kp2pml30@gmail.com>
2026-09-13 14:43:22 +0900

    chore(deps): rebase wasmtime onto v48.0.1 ⬆️
    Wasmtime 48 is an LTS release supported for 24 months. wasm-tools moves to v1.254.0 in lockstep because Wasmtime pins wasmparser 0.254.

--- executor/src/rt/supervisor/mod.rs
+++ executor/src/rt/supervisor/mod.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::{BTreeMap, HashSet},
+    num::NonZeroUsize,
     sync::{atomic::AtomicU32, Arc},
 };
 
@@ -120,10 +121,12 @@ pub fn create_engines(
     det_conf
         .wasm_floats_enabled(false)
         .cranelift_nan_canonicalization(true)
-        .wasm_backtrace(true);
+        .wasm_backtrace_max_frames(NonZeroUsize::new(20));
 
     let mut non_det_conf = base_conf.clone();
-    non_det_conf.wasm_floats_enabled(true).wasm_backtrace(false);
+    non_det_conf
+        .wasm_floats_enabled(true)
+        .wasm_backtrace_max_frames(None);
 
     let det_engine = wasmtime::Engine::new(&det_conf)
         .map_err(crate::wasmtime_to_anyhow)
